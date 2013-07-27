9e060b5a6a339df13a07fda381867c54a35ba99c
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2013-07-27 10:43:04 -0400

    Add another test

--- test/unit/gateways/paypal_express_test.rb
+++ test/unit/gateways/paypal_express_test.rb
@@ -298,6 +298,19 @@ class PaypalExpressTest < Test::Unit::TestCase
     assert_equal '-8', amounts[1].text
   end
 
+  def test_non_fractional_discounts_are_correctly_calculated_with_jpy_currency
+    xml = REXML::Document.new(@gateway.send(:build_setup_request, 'SetExpressCheckout', 14300, { :items =>
+                            [{:name => 'item one', :description => 'description', :amount => 15000, :number => 1, :quantity => 1},
+                             {:name => 'Discount', :description => 'Discount', :amount => -700, :number => 2, :quantity => 1}],
+                             :subtotal => 14300, :currency => 'JPY', :shipping => 0, :handling => 0, :tax => 0 }))
+
+    assert_equal '143', REXML::XPath.first(xml, '//n2:OrderTotal').text
+    assert_equal '143', REXML::XPath.first(xml, '//n2:ItemTotal').text
+    amounts = REXML::XPath.match(xml, '//n2:Amount')
+    assert_equal '150', amounts[0].text
+    assert_equal '-7', amounts[1].text
+  end
+
   def test_fractional_discounts_are_correctly_calculated_with_usd_currency
     xml = REXML::Document.new(@gateway.send(:build_setup_request, 'SetExpressCheckout', 14250, { :items =>
                             [{:name => 'item one', :description => 'description', :amount => 15000, :number => 1, :quantity => 1},
